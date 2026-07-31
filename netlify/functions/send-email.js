c279861afdc40ab7870ce6469e2dfc13ffa30d37
The C Point <emma@bengomedia.com>
2026-07-31 17:16:11 +0100

    Update send-email.js

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,1 +1,39 @@
+exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+      },
+      body: ''
+    };
+  }
 
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method not allowed' };
+  }
+
+  const RESEND_KEY = process.env.RESEND_KEY;
+  const FROM_EMAIL = process.env.FROM_EMAIL || 'onboarding@resend.dev';
+  if(!RESEND_KEY) return { statusCode: 500, body: JSON.stringify({ error: 'RESEND_KEY not configured' }) };
+
+  const { to, subject, html } = JSON.parse(event.body);
+
+  const res = await fetch('https://api.resend.com/emails', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + RESEND_KEY,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ from: FROM_EMAIL, to: [to], subject, html })
+  });
+
+  const data = await res.json();
+
+  return {
+    statusCode: res.status,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
